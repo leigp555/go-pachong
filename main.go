1a0b2de674062066c200e6abe70f03fafe6e4564
leigp <907090585@qq.com>
2022-10-20 23:33:14 +0800

    update

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"net/http"
 )
 
-
-func crawler(){
+func crawler() {
 	//构造请求
-	client:= http.Client{}
+	client := http.Client{}
 	req, err := http.NewRequest("GET", "https://tool.chinaz.com/tools/use", nil)
-	if err!=nil{
-		fmt.Println("请求构建失败",err)
+	if err != nil {
+		fmt.Println("请求构建失败", err)
 	}
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Pragma", "no-cache")
@@ -24,14 +23,14 @@ func crawler(){
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
 	//发送请求
 	resp, err := client.Do(req)
-	if err!=nil{
-		fmt.Println("请求失败",err)
+	if err != nil {
+		fmt.Println("请求失败", err)
 	}
 	defer resp.Body.Close()
 	//解析网页
 	reader, err := goquery.NewDocumentFromReader(resp.Body)
-    if err!=nil{
-		fmt.Println("解析失败",err)
+	if err != nil {
+		fmt.Println("解析失败", err)
 	}
 	//获取节点信息
 	//body > div.Tool-MainWrap.wrapper.mb10 > div.CorContent.auto
@@ -47,12 +46,12 @@ func crawler(){
 	text := reader.Find("body > div.Tool-MainWrap.wrapper.mb10 > div.CorContent.auto > div:nth-child(7)").Text()
 	var arr []string
 	arr = append(arr, text)
-	for index,value:=range arr{
-		fmt.Printf("%v:%v\n",index,value)
+	for index, value := range arr {
+		fmt.Printf("%v:%v\n", index, value)
 	}
 }
 
-func main()  {
+func main() {
 	crawler()
-	fmt.Println("ok")
+	fmt.Println("完成")
 }
